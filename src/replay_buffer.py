cf7e809c5bbab2d8c6a7fe0a06773cc6ee38c7b9
Ran Wright <ranman37@gmail.com>
2025-03-15 12:26:30 +0200

    plots & Charts (#32)
    * plots & Charts
    
    * stacking frames
    
    * plots

--- src/replay_buffer.py
+++ src/replay_buffer.py
@@ -6,12 +6,24 @@ class ReplayBuffer:
     def __init__(self, config: dict):
         buffer_size = config["buffer_size"]
         batch_size = config["batch_size"]
+        frame_stack_size = 4  # Number of frames per state
         self.buffer = deque(maxlen=buffer_size)
+        self.frame_stack = deque(maxlen=frame_stack_size)
         self.buffer_size = buffer_size
         self.batch_size = batch_size
 
     def add(self, state, action, reward, next_state, done):
-        self.buffer.append((state, action, reward, next_state, done))
+        """Stack frames before adding to replay buffer"""
+        if len(self.frame_stack) < self.frame_stack.maxlen:
+            for _ in range(self.frame_stack.maxlen - len(self.frame_stack)):
+                self.frame_stack.append(state)  # Fill with initial frame
+        self.frame_stack.append(state)
+        stacked_state = np.array(self.frame_stack)
+
+        self.frame_stack.append(next_state)
+        stacked_next_state = np.array(self.frame_stack)
+
+        self.buffer.append((stacked_state, action, reward, stacked_next_state, done))
 
     def sample(self, batch_size):
         indices = np.random.choice(len(self.buffer), batch_size, replace=False)
